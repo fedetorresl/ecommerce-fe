3897aac9b9e03e31a8880bdcb8cbb97873f76770
Federico Torres <151167352+fedetorresl@users.noreply.github.com>
2024-01-04 10:44:20 -0300

    Review fixes

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,23 +1,25 @@
-import { useFavorites } from "../hooks/useFavorites";
 import { icons } from "./common";
 import { Product } from "../api/products";
 import { useFavoritesStore } from "../stores";
 
 export const ProductCard = ({ product }: { product: Product }) => {
-  const favoritesStore = useFavoritesStore();
+  const { favorites, setFavorites } = useFavoritesStore();
   const isProductInFavorites = useFavoritesStore().favorites.some(
     (favProduct) => favProduct.id === product.id
   );
+  const FavoritesIcon = isProductInFavorites
+    ? icons.FilledProductHeartIcon
+    : icons.ProductHeartIcon;
 
   const handleHeartClick = () => {
     if (isProductInFavorites) {
-      const updatedFavorites = favoritesStore.favorites.filter(
+      const updatedFavorites = favorites.filter(
         (favProduct) => favProduct.id !== product.id
       );
-      favoritesStore.setFavorites(updatedFavorites);
+      setFavorites(updatedFavorites);
     } else {
-      const updatedFavorites = [...favoritesStore.favorites, product];
-      favoritesStore.setFavorites(updatedFavorites);
+      const updatedFavorites = [...favorites, product];
+      setFavorites(updatedFavorites);
     }
   };
 
@@ -41,11 +43,7 @@ export const ProductCard = ({ product }: { product: Product }) => {
           € {product.price}
         </p>
         <button className="" onClick={handleHeartClick}>
-          {isProductInFavorites ? (
-            <icons.FilledProductHeartIcon className="cursor-pointer h-6 w-6" />
-          ) : (
-            <icons.ProductHeartIcon className="cursor-pointer h-6 w-6" />
-          )}
+          <FavoritesIcon className="cursor-pointer h-6 w-6" />
         </button>
       </div>
     </div>
